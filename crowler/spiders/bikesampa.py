5fc109169d3d7439847fc4264e94cd305a14bd90
Josimar Lemos <josimar@voltis.com.br>
2019-06-03 13:41:02 -0300

    First commit

---
+++ crowler/spiders/bikesampa.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BikesampaSpider(scrapy.Spider):
+    name = 'bikesampa'
+    allowed_domains = ['bikeitau.com.br']
+    start_urls = ['http://bikeitau.com.br/bikesampa/']
+
+    def parse(self, response):
+        lat = self.fetch(response, 'lat_arr')
+        lng = self.fetch(response, 'long_arr')
+
+        for station in self.merge(lat, lng):
+            yield station
+
+
+    def fetch(self, response, field):
+        query = "//input[@id='{}']/@value".format(field)
+
+        result = response.xpath(query).get()
+        
+        return [item.strip() for item in result.split(';')]
+
+    def merge(self, lat, lng):
+        return [{'lat': lat, 'lng': lng} for lat, lng in zip(lat, lng)]
