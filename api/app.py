2027824599bb180357be947f70a40b28db6a1ed9
Josimar Lemos <josimar@voltis.com.br>
2019-06-05 13:46:24 -0300

    Add endpoint to list bike stations

---
+++ api/app.py
@@ -0,0 +1,23 @@
+import os
+import json
+import redis
+from flask import Flask
+
+
+app = Flask(__name__)
+
+url = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
+redis = redis.Redis.from_url(url)
+
+@app.route('/api/stations')
+def stations():
+    keys = [
+        key.decode().split(':').pop()
+        for key in redis.keys('services:*')
+    ]
+
+    keys.sort()
+    
+    key = 'services:{}'.format(keys.pop())
+
+    return redis.get(key)
