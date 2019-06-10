aeea70c0ace2a411a899a435584599c443cb96fc
Josimar Lemos <josimar@voltis.com.br>
2019-06-10 17:43:53 -0300

    Setup to store bike stations data in Redis

--- crowler/pipelines.py
+++ crowler/pipelines.py
@@ -6,6 +6,20 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class CrowlerPipeline(object):
+import os
+import json
+import redis
+
+
+class RedisPipeline(object):
+    def __init__(self):
+        url = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
+        self.redis = redis.Redis.from_url(url)
+
     def process_item(self, item, spider):
+        created_at = item['created_at']
+        key = "services:{}".format(created_at)
+        
+        self.redis.setex(key, 60*60, json.dumps(dict(item)))
+
         return item
--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 import os
 import json
+import redis
 from flask import Flask, make_response, jsonify
 
 
 app = Flask(__name__)
 
+url = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
+redis = redis.Redis.from_url(url)
+
 @app.route('/api/stations')
 def stations():
-    filename = os.path.join(app.static_folder, 'stations.json')
-    
-    with open(filename, 'r') as stations:
-        data = json.load(stations)
+    keys = [
+        key.decode().split(':').pop()
+        for key in redis.keys('services:*')
+    ]
 
-    return make_response(jsonify(data))
+    keys.sort()
+    
+    key = 'services:{}'.format(keys.pop())
+
+    return redis.get(key)
